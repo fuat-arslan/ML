b4fd643e8cf42660ca388c429b739de7b4668c56
fuat <arsln.fuat@gmail.com>
2022-11-08 16:13:30 +0000

    MinMax

--- Preprocess/Preprocess.py
+++ Preprocess/Preprocess.py
@@ -29,24 +29,17 @@ class pd_np_Converter():
             return pd.DataFrame(self.X,self.columns)
 
 
-
-
-    
-
-
-
-
 #normalization
 
 class Normalizer():
     """
     It calculates the mean and standard deviation of the each future.
-
+    It subrtacts the mean and divide by std to normalize.
     """
     def __init__ (self):
         pass
     
-    def calc(self,X):
+    def learn(self,X):
         self.MEAN = np.mean(X,axis=0) #Calculate the mean 
         self.STD = np.std(X,axis=0) #Calculate the STD of each feature
         return self
@@ -56,9 +49,31 @@ class Normalizer():
         return output
 
     def fast(self,X):
-        return self.calc(X).execute(X)
+        return self.learn(X).execute(X)
     
+#MinMax
 
+class MinMax():
+    """
+    It maps the values to a ceratin interval.
+
+    """
+    def __init__(self,low = 0, high = 1):
+        self.low_ = low
+        self.high_ = high
+
+    def learn(self,X):
+        self.MIN = np.min(X,axis=0)
+        self.MAX = np.max(X,axis=0)
+        return self
+    
+    def execute(self,X):
+        X_scaled = (X-self.MIN)/(self.MAX -self.MIN) #X features are mapped (0,1)
+        X_rescaled = X_scaled * (self.high_-self.low_) + self.low_
+        return X_rescaled
+    
+    def fast(self,X):
+        return self.learn(X).execute(X)
 
 
 
