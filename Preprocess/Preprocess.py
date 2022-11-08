0cc1b45712de3b7ad42ccbb20bf2a16c41a35acf
fuat <arsln.fuat@gmail.com>
2022-11-08 15:41:18 +0000

    Normalzier

--- Preprocess/Preprocess.py
+++ Preprocess/Preprocess.py
@@ -1,8 +1,64 @@
+"""
+This scirpt includes function for preprocessing.
+"""
+
 import numpy as np
+import pandas as pd
+#Pandas Numpy converter
+
+class pd_np_Converter():
+
+    def __init__ (self,X,col = None):
+        self.X = X
+        if isinstance(X, pd.DataFrame):
+            self.pandas_inst = True
+            self.columns = X.columns
+        else :
+            self.pandas_inst = False
+            self.columns = col
+    
+    def to_nup(self):
+        if self.pandas_inst:
+            return self.X.to_numpy()
+        else:
+            return self.X
+    def to_pd(self):
+        if self.pandas_inst:
+            return self.X
+        else:
+            return pd.DataFrame(self.X,self.columns)
 
 
-arr = np.loadtxt("./archive/sdss-IV-dr16-70k.csv",
-                 delimiter=",", dtype=str)
-display(arr)
+
+
+    
+
+
+
+
+#normalization
+
+class Normalizer():
+    """
+    It calculates the mean and standard deviation of the each future.
+
+    """
+    def __init__ (self):
+        pass
+    
+    def calc(self,X):
+        self.MEAN = np.mean(X,axis=0) #Calculate the mean 
+        self.STD = np.std(X,axis=0) #Calculate the STD of each feature
+        return self
+
+    def execute(self,X):
+        output = (X - self.MEAN)/self.STD
+        return output
+
+    def fast(self,X):
+        return self.calc(X).execute(X)
+    
+
+
 
 
