43245f045114484a81fcac0a667f323d325c76f0
fuat-arslan <115395223+fuat-arslan@users.noreply.github.com>
2022-11-20 14:39:04 +0300

    Wieghted KNN added

--- Models/KNN.py
+++ Models/KNN.py
@@ -1,6 +1,6 @@
 import numpy as np 
 class KNN():
-    def __init__(self, X, Y, num_neig, metric ="euclidian" ):
+    def __init__(self, X, Y, num_neig, metric ="euclidian" ,weighted =False):
         self.X = X 
         self.Y = Y
         self.num_neig = num_neig
@@ -15,6 +15,7 @@ class KNN():
         return self
     
     def predict(self, sample):
+        eps = 1e-13
         predictions = []
         for j in range(len(sample)):
             sample_hat = np.tile(sample[j], (len(self.X), 1))
@@ -22,8 +23,15 @@ class KNN():
             #print(sample_hat.shape)
             dist = self.distance_metric(self.metric, self.X, sample_hat)
             sorted_idx = np.argsort(dist)
-            #print(sorted_idx)
-            #print(self.Y[sorted_idx[:self.num_neig]])
-            #print(np.bincount(int(self.Y[sorted_idx[:self.num_neig]])))
-            predictions.append(np.bincount(self.Y[sorted_idx[:self.num_neig]].astype(int).reshape(-1)).argmax())
+            if weighted:
+                u, indicies = np.unique(self.Y[sorted_idx[:self.num_neig]],return_inverse=True)
+                indexed_distances = dist[sorted_idx[:self.num_neig]]
+                weighted_label = np.zeros(len(np.unique(self.Y)))
+                for k in range(len(indicies)):
+                    weighted_label[indicies[k]] += 1/(indexed_distances[k]+eps)
+
+                #print('predicition',u[np.argmax(weighted_label)])
+                predictions.append(u[np.argmax(weighted_label)])
+            else:
+                predictions.append(np.bincount(self.Y[sorted_idx[:self.num_neig]].astype(int).reshape(-1)).argmax())
         return predictions  
