87c59b90de7d4c9839dbbea0a9243f6d83f4b73d
fuat-arslan <115395223+fuat-arslan@users.noreply.github.com>
2022-12-17 12:09:56 +0000

    dd

--- Models/NeuralNetwork/Layers.py
+++ Models/NeuralNetwork/Layers.py
@@ -28,7 +28,7 @@ class Dense():
         if self.activation == "softmax":
             A = softmax(self.Z)
         
-        elif self.activation == "softmax_stable":
+        elif self.activation == "stable_softmax":
             A = stable_softmax(self.Z)
         
         elif self.activation == "relu":
